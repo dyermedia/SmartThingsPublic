99c6a3609db2a1304927d42bb6ade33b59573c04
claytonjn <claytonjn@users.noreply.github.com>
2016-04-24 23:02:03 -0400

    Further GitHub integration testing

--- smartapps/claytonjn/outdoor-lighting.src/outdoor-lighting.groovy
+++ smartapps/claytonjn/outdoor-lighting.src/outdoor-lighting.groovy
@@ -1,23 +1,8 @@
-/**
- *  Outdoor Lighting: SunriseSunset
- *
- *  Copyright 2016 Clayton Nummer
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
 definition(
-    name: "Outdoor Lighting: Sunrise/Sunset",
+    name: "Outdoor Lighting",
     namespace: "claytonjn",
     author: "claytonjn",
-    description: "Turns on the outside lights at sunset, turns them off at sunrise.",
+    description: "Turns on the outside lights at sunset\, turns them off at sunrise.",
     category: "My Apps",
     iconUrl: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn.png",
     iconX2Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@2x.png",
@@ -25,13 +10,9 @@ definition(
 
 
 preferences {
-	page(name: "page", install: true, uninstall: true) {
-        section("Title") {
-        	paragraph "Turns on the outside lights at sunset, turns them off at sunrise."
-            input "lights", "capability.switch", title: "Light(s)", multiple: true
-            input("recipients", "contact", title: "Send notifications to", multiple: true)
-        }
-    }
+
+  paragraph "Turns on the outside lights at sunset\, turns them off at sunrise."
+
 }
 
 def installed() {
@@ -48,9 +29,7 @@ def updated() {
 }
 
 def initialize() {
-    subscribe(location, "sunset", sunsetHandler)
-	subscribe(location, "sunrise", sunriseHandler)
-    subscribe(app, appHandler)
+
 }
 
 
