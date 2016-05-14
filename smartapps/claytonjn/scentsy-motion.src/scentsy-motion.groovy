a1283dcadd716b9545f8a278ca918fb9ade1fe2b
Clayton Nummer <claytonjn@gmail.com>
2016-05-14 12:10:57 -0400

    Add initial version of Scentsy Motion SmartApp

---
+++ smartapps/claytonjn/scentsy-motion.src/scentsy-motion.groovy
@@ -0,0 +1,58 @@
+/**
+ *  Scentsy Motion
+ *
+ *  Copyright 2016 Clayton Nummer
+ *
+ */
+definition(
+    name: "Scentsy Motion",
+    namespace: "claytonjn",
+    author: "Clayton Nummer",
+    description: "Turn on Scentsy(s) when there's motion.",
+    category: "My Apps",
+    iconUrl: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn.png",
+    iconX2Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@3x.png")
+
+
+preferences {
+	page(name: "page", install: true, uninstall: true) {
+        section("Preferences") {
+            paragraph "Turn on Scentsy(s) when there's motion."
+            input "scentsys", "capability.switch", title: "Scentsy(s)", multiple: true
+            input "motions", "capability.motionSensor", title: "Motion Sensor(s)", multiple: true
+        }
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	subscribe(settings.motions, "motion", motionHandler)
+}
+
+void motionHandler(evt) {
+	if (location.mode in ["Home"]){
+        if (evt.value == "active") {
+            unschedule(scentsysOff)
+            settings.scentsys?.on()
+        } else if (evt.value == "inactive") {
+            runIn(60*15, scentsysOff)
+        }
+    }
+}
+
+void scentsysOff() {
+	settings.scentsys?.off()
+}
