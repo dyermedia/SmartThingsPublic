c2ab0401a6fcce495370840cbb1296ea190a9239
claytonjn <claytonjn@gmail.com>
2016-06-23 09:34:39 -0400

    Add Trigger Alert Mode SmartApp

---
+++ smartapps/claytonjn/trigger-alert-mode.src/trigger-alert-mode.groovy
@@ -0,0 +1,56 @@
+/**
+ *  Trigger Alert Mode
+ *
+ *  Copyright 2016 Clayton Nummer
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Trigger Alert Mode",
+    namespace: "claytonjn",
+    author: "claytonjn",
+    description: "Changes mode to Alert if one of the \"Alert\" virtual switches is on.",
+    category: "My Apps",
+    iconUrl: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn.png",
+    iconX2Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@3x.png")
+
+
+preferences {
+	page(name: "page", install: true, uninstall: true) {
+        section("Preferences") {
+            paragraph "Changes mode to Alert if one of the \"Alert\" virtual switches is on."
+            input "switches", "capability.switch", title: "\"Alert\" Switch(s)", multiple: true
+        }
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+    subscribe(settings.switches, "switch.on", switchOnHandler)
+}
+
+void switchOnHandler(evt) {
+	location.setMode("Alert")
+    sendNotificationEvent("Mode changed to Alert!")
+}
