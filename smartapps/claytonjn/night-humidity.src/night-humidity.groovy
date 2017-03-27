ee4d145911d18bf2951fe493a0c60fc970edf743
claytonjn <claytonjn@gmail.com>
2017-03-27 12:19:49 -0400

    Allow for specifying humidity setpoint in settings

--- smartapps/claytonjn/night-humidity.src/night-humidity.groovy
+++ smartapps/claytonjn/night-humidity.src/night-humidity.groovy
@@ -5,10 +5,10 @@
  *
  */
 definition(
-    name: "Night Humidity",
+    name: "Humidity Maintainer",
     namespace: "claytonjn",
     author: "Clayton Nummer",
-    description: "Controls a Humidifier to maintain 50% RH.",
+    description: "Controls a Humidifier to maintain relative humidity level.",
     category: "My Apps",
     iconUrl: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn.png",
     iconX2Url: "https://raw.githubusercontent.com/claytonjn/SmartThingsPublic/claytonjn-personal/icons/claytonjn@2x.png",
@@ -18,9 +18,10 @@ definition(
 preferences {
 	page(name: "page", install: true, uninstall: true) {
         section("Preferences") {
-        	paragraph "Controls a Humidifier to maintain 50% RH."
+        	paragraph "Controls a Humidifier to maintain relative humidity level."
             input "humiditys", "capability.relativeHumidityMeasurement", title: "Humidy Sensor(s)", multiple: true
             input "humidifiers", "capability.switch", title: "Humidifier(s)", multiple: true
+            input "relativeHumidity", "number", title: "Relative Humidity to Maintain", defaultValue: 50, required: true
             mode(title: "Set for specific mode(s)")
             label title: "Assign a name", required: false
         }
@@ -51,7 +52,7 @@ void evtHandler(evt) {
         avgHumidity += humidity.currentValue("humidity")
     }
     avgHumidity = avgHumidity / settings.humiditys.size()
-    if (avgHumidity < 50) {
+    if (avgHumidity < settings.relativeHumidity) {
         settings.humidifiers?.on()
     } else {
         settings.humidifiers?.off()
